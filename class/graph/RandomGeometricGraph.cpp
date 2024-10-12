e6460ad2f2abeca38e9bf83d6a026f5fb45f614e
NilCasas <nil.cases@gmail.com>
2024-10-12 13:37:34 +0200

    RGG_falta_testear

--- class/graph/RandomGeometricGraph.cpp
+++ class/graph/RandomGeometricGraph.cpp
@@ -12,25 +12,17 @@ class RandGeomGraph : public Graph {//solo haremos edgePercolation()
 
 //Sea G = (V,E)
 //∀ u,v ∈ V, u~v <-> distanciaEuclidiana(u,v) <= radio
-
-/* como debería calcular r debería pasarla como parámetro
- con la creadora? O sino por una n determinada generar aleatoriamente
- una r entre ciertos valores determinados [x,y]?
- se dice que r ~ sqrt(ln(n)/n))
- */
-
 private:
     double r; 
     vector<pair<double,double>> plano;//indica coordenadas ∈ [0,1) de cada nodo i  
 public:
-    RandGeomGraph() : Graph(){ //Constructor por defecto
+    RandGeomGraph(){ //Constructor por defecto
         RandGeomGraph(0);
     }
     
-    RandGeomGraph(int n) : Graph() { 
+    RandGeomGraph(int n) { 
         this->n = n;
         this->r = sqrt(log(n)/n);
-        //¿MEJOR generar n random cercana a este valor tipo, pe r = ans +=[-0.1,+0.1]?
         exist.resize(n*n, true);//inútil, no eliminaremos nunca vértices
         plano.resize(n);
         adyacencias = vector<set<int>>(n*n);
@@ -42,7 +34,7 @@ public:
             plano[i] = make_pair(x, y);
         }
         // Conectar los nodos si la distancia euclidiana entre ellos es <= r
-        for (int i = 0; i < n; ++i) {//O(n^2)
+        for (int i = 0; i < n; ++i) {
             for (int j = 0; j < n; ++j) {
                 if (j != i) {
                     if (distancia(plano[i], plano[j]) <= r) {
@@ -58,13 +50,12 @@ public:
     double distancia(const pair<double, double>& p1, const pair<double, double>& p2) const {
         return sqrt(pow(p1.first - p2.first, 2) + pow(p1.second - p2.second, 2));
     }
-        // Función para imprimir el grafo
-    virtual void printGraph() const override { // Override de la función printGraph
+    // Función para imprimir el grafo con el radio y los puntos de cada vértice, 
+    void printRandGeomGraph() const { // Forma alternativa a printGraph, aunque printGraph funciona igualmente
         cout << "Grafo con " << n << " vertices y radio = " << r << "):\n";
         for (int i = 0; i < n; ++i) {
             if (exist[i]) {
-                cout << "Vertex " << i << " ("
-                     << plano[i].first << ", " << plano[i].second << "):";
+                cout << "Vertex " << i << " (" << plano[i].first << ", " << plano[i].second << "):";
                 for (int neighbor : adyacencias[i]) {
                     cout << " " << neighbor;
                 }
